3bb2e89dc645ed84d9eb0eb2b7a6107f786a40bc
Sean Narron <seannarron9@gmail.com>
2022-10-04 15:10:31 -0700

    Modified.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
