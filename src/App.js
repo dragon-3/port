0eb0bb2ca6a85eefa52d8c43001baf5fee69d529
Sean Narron <seannarron9@gmail.com>
2022-10-05 15:07:18 -0700

    Modified.

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
 
